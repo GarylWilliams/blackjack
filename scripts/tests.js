41cf6394ac28a9aa5d113266cc8b2699469a9684
Gary Williams <williamsgaryl789@gmail.com>
2017-06-13 14:11:13 -0400

    First chance

---
+++ scripts/tests.js
@@ -0,0 +1,64 @@
+(function () {
+
+  'use strict';
+    
+   function handValue(hand) {
+    let value = 0; 
+    
+    hand.sort(card => card === 'A'?1:-1); 
+    
+    for (let i = 0; i < hand.length; i++) {
+    if (hand[i] === '2' ||
+        hand[i] === '3' ||
+        hand[i] === '4' ||
+        hand[i] === '5' ||
+        hand[i] === '6' ||
+        hand[i] === '7' ||
+        hand[i] === '8' ||
+        hand[i] === '9' ||
+        hand[i] === '10') {
+        value += parseInt(hand[i]);
+        }    
+    else if(hand[i] === "K" || hand[i] === "Q" || hand[i] ==="J") {
+        value += 10; 
+    } else {
+        if (value > 11) {
+        value += 1;     
+        }
+    }        
+      return (value);  
+  }
+}
+
+handValue("2", "2", "K"); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+  // Assert Function
+  function assert(hand, value) {
+    let output = handValue(hand);
+    console.assert(output === value, `Expecting ${output} to be ${value}`);
+  }
+
+  // Blackjack Hand Tests
+  // Hand, Value
+  assert(["2", "2", "8"], 12);
+  assert(["2", "2", "K"], 14);
+  assert(["2", "Q"], 12);
+  assert(["7", "J"], 17);
+  assert(["7", "A"], 18);
+  assert(["8", "J", "A"], 19);
+  assert(["8", "A", "J"], 19);
+  assert(["8", "7", "A", "A"], 17);
+
+})();
